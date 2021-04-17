48c4c06f68fdbfbc7eb59ee25d79c9cf66ad8a3b
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-04-17 13:57:42 -0500

    Created image and song handlers for files

---
+++ social/src/main/java/com/music/social/config/ImageConfig.java
@@ -0,0 +1,19 @@
+package com.music.social.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ImageConfig implements WebMvcConfigurer{
+
+    @Value("${image-upload-path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/" + "**")
+                .addResourceLocations("file:" + uploadPath);
+    
+}
---
+++ social/src/main/java/com/music/social/config/SongConfig.java
@@ -0,0 +1,18 @@
+package com.music.social.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SongConfig implements WebMvcConfigurer {
+    
+    @Value("${song-upload-path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/songs/" + "**")
+                .addResourceLocations("file:" + uploadPath);
+
+}
